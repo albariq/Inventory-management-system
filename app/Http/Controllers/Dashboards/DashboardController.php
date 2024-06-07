e2badc9ad86f0c0041415852af8ae8d22c1f606b
albariq <albariq00@gmail.com>
2024-06-08 04:40:06 +0700

    remove chart & add card dashboard

--- app/Http/Controllers/Dashboards/DashboardController.php
+++ app/Http/Controllers/Dashboards/DashboardController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers\Dashboards;
 
-use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\Category;
 use App\Models\Purchase;
 use App\Models\Quotation;
-use App\Models\Category;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $today = Carbon::today();
+        $firstDayOfMonth = Carbon::now()->firstOfMonth();
+
         $orders = Order::where("user_id", auth()->id())->count();
         $products = Product::where("user_id", auth()->id())->count();
         $purchases = Purchase::where("user_id", auth()->id())->count();
@@ -42,6 +46,18 @@ class DashboardController extends Controller
             ->select('name as product_name', 'quantity as stock')
             ->get();
 
+        // Query omset dan laba harian
+        $dailySales = DB::table('orders')
+        ->select(DB::raw('SUM(total) as daily_revenue, SUM(pay) as daily_profit'))
+        ->whereDate('order_date', $today)
+        ->first();
+
+        // Query Omset dan laba bulanan
+        $monthlySales = DB::table('orders')
+            ->select(DB::raw('SUM(total) as monthly_revenue, SUM(pay) as monthly_profit'))
+            ->whereBetween('order_date', [$firstDayOfMonth, Carbon::now()])
+            ->first();
+
         return view('dashboard', [
             'products' => $products,
             'orders' => $orders,
@@ -54,7 +70,11 @@ class DashboardController extends Controller
             'quotations' => $quotations,
             'barChartData' => $barChartData,
             'pieChartData' => $pieChartData,
-            'stockPerProductData' => $stockPerProductData // Menambahkan data stok per produk
+            'stockPerProductData' => $stockPerProductData, // Menambahkan data stok per produk
+            'dailyRevenue' => $dailySales->daily_revenue,
+            'dailyProfit' => $dailySales->daily_profit,
+            'monthlyRevenue' => $monthlySales->monthly_revenue,
+            'monthlyProfit' => $monthlySales->monthly_profit
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,30 +31,6 @@
     <div class="page-body">
         <div class="container-xl">
             <div class="row row-deck row-cards">
-                <div class="container">
-                    <h2>Bar Chart: Jumlah Stok per Kategori Produk</h2>
-                    <canvas id="barChart"></canvas>
-            
-                    <h2>Pie Chart: Distribusi Persentase Kategori Produk</h2>
-                    <canvas id="pieChart"></canvas>
-            
-                    <h2>Bar Chart: Stok per Produk</h2>
-                    <canvas id="stockPerProductChart"></canvas>
-            
-                    <div class="row">
-                        <div class="col">
-                            <div class="font-weight-medium">
-                                {{ $products }} Products
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="font-weight-medium">
-                                {{ $orders }} Orders
-                            </div>
-                        </div>
-                        <!-- Tambahkan elemen lain sesuai kebutuhan -->
-                    </div>
-                </div>
                 {{-- <div class="col-sm-6 col-lg-3">
                     <div class="card">
                         <div class="card-body">
@@ -320,6 +296,143 @@
                                             </div>
                                             <div class="text-muted">
                                                 {{ $todayQuotations }} today
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                -
+                <div class="col-12">
+                    <div class="row row-cards">
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-primary text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-packages" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M7 16.5l-5 -3l5 -3l5 3v5.5l-5 3z" />
+                                                    <path d="M2 13.5v5.5l5 3" />
+                                                    <path d="M7 16.545l5 -3.03" />
+                                                    <path d="M17 16.5l-5 -3l5 -3l5 3v5.5l-5 3z" />
+                                                    <path d="M12 19l5 3" />
+                                                    <path d="M17 16.5l5 -3" />
+                                                    <path d="M12 13.5v-5.5l-5 -3l5 -3l5 3v5.5" />
+                                                    <path d="M7 5.03v5.455" />
+                                                    <path d="M12 8l5 -3" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                                Laba Harian
+                                            </div>
+                                            <div class="text-muted">
+                                                {{ number_format($dailyProfit, 0, ',', '.') }} IDR
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                    <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                    <path d="M17 17h-11v-14h-2" />
+                                                    <path d="M6 5l14 1l-1 7h-13" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                                Omset Harian
+                                            </div>
+                                            <div class="text-muted">
+                                                {{ number_format($dailyRevenue, 0, ',', '.') }} IDR
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-twitter text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-twitter -->
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-truck-delivery" width="24"
+                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                    <path d="M5 17h-2v-4m-1 -8h11v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" />
+                                                    <path d="M3 9l4 0" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                                Laba Bulanan
+                                            </div>
+                                            <div class="text-muted">
+                                                {{ number_format($monthlyProfit, 0, ',', '.') }} IDR
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="card card-sm">
+                                <div class="card-body">
+                                    <div class="row align-items-center">
+                                        <div class="col-auto">
+                                            <span
+                                                class="bg-facebook text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-facebook -->
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-files" width="24" height="24"
+                                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M15 3v4a1 1 0 0 0 1 1h4" />
+                                                    <path
+                                                        d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
+                                                    <path
+                                                        d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                        <div class="col">
+                                            <div class="font-weight-medium">
+                                                Omset Bulanan
+                                            </div>
+                                            <div class="text-muted">
+                                                {{ number_format($monthlyRevenue, 0, ',', '.') }} IDR
                                             </div>
                                         </div>
                                     </div>
